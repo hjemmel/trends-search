ee18d4f769ad42d145ecd14348bac3abbb56a09a
Hans Jakob Emmel <hans.emmel@gmail.com>
2018-06-17 18:26:59 +1200

    Define back-end architecture

---
+++ trends-search-server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const trendsRouter = require('./routes/trends');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/trends', trendsRouter);
+
+module.exports = app;
