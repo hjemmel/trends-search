4cc583a91dd63ebca158e2bfbf1daa3463406ea0
Hans Jakob Emmel <hans.emmel@gmail.com>
2018-06-17 18:26:48 +1200

    Define back-end architecture

---
+++ trends-search-server/controllers/trends.js
@@ -0,0 +1,13 @@
+const Promise = require('promise');
+
+module.exports = {
+  getTrends
+};
+
+function getTrends() {
+  const result = {"default": {"rankedList": {"rankedKeyword":[{"query":"dog show 2016","value":100,"formattedValue":"100","link":"/"},{"query":"2016 westminster dog show","value":95,"formattedValue":"95","link":"/"}]}}};
+
+  return new Promise((resolve, reject) => {
+     resolve(result);
+  });
+}
---
+++ trends-search-server/routes/trends.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const trendsController = require('../controllers/trends');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    trendsController.getTrends().then(
+        result => res.status(200).json(result),
+        err => err
+    )
+});
+
+module.exports = router;
