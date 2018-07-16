9ee128dbd3ba4263425710cd0c6b2a08118c0c44
Hans Jakob Emmel <hans.emmel@gmail.com>
2018-07-17 01:12:33 +1200

    adding first react scripts

---
+++ trends-search-ui/src/components/Header/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import {
+  Alignment,
+  Classes,
+  Button,
+  Navbar,
+  NavbarGroup,
+  NavbarHeading
+} from '@blueprintjs/core';
+
+class Header extends React.Component {
+  render (props) {
+    const classes = classNames(Classes.DARK, Classes.FIXED_TOP);
+    return (
+      <div className={this.props.className}>
+        <Navbar className={classes}>
+          <NavbarGroup align={Alignment.LEFT}>
+            <NavbarHeading>Blueprint</NavbarHeading>
+          </NavbarGroup>
+          <NavbarGroup align={Alignment.RIGHT}>
+            <Button className={Classes.MINIMAL} icon='user' />
+            <Button className={Classes.MINIMAL} icon='notifications' />
+            <Button className={Classes.MINIMAL} icon='cog' />
+          </NavbarGroup>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {
+  className: PropTypes.string
+};
+
+export default Header;
---
+++ trends-search-ui/src/containers/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+  <div className='home'>
+    <div className='main'>hans</div>
+    <div className='options' />
+  </div>
+);
+
+export default Home;
